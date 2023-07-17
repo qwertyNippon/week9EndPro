e12ac74c6ecddd8f30ec49b13d66c17083ec632b
qwertyNippon <orlandohenry100@gmail.com>
2023-07-16 22:46:42 -0400

    added updates

--- src/views/MyCart.jsx
+++ src/views/MyCart.jsx
@@ -1,11 +1,14 @@
 import { React, useContext } from 'react'
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card';
 import './MyCart.css'
 
 const MyCart = () => {
-    // const { cart, setCart } = useContext(DataContext);
+    const { cart, setCart } = useContext(DataContext);
     /*
     The plan:
     1. grab// modify cart ---X
@@ -14,63 +17,69 @@ const MyCart = () => {
     4. increment item ---X
     5. decrement item
     */
-//    const clearCart = () => {
-//     setCart({size:0, total:0, movies:{}});
-//    }
+   const clearCart = () => {
+    setCart({size:0, total:0, product:{}});
+   }
 //    const increaseItem = id => {
 //     let copyCart = {...cart};
 //     console.log(copyCart);
 //     copyCart.size ++;
 
-//     copyCart.total += (Math.round(copyCart.movies[id].data.price * 100) / 100);
-//     copyCart.movies[id].quantity ++;
+//     copyCart.total += (Math.round(copyCart.Products[id].data.price * 100) / 100);
+//     copyCart.Products[id].quantity ++;
 //     setCart(copyCart);
 //    }
 //    const decreaseItem = id => {
 //     let copyCart = {...cart};
 //     copyCart.size --;
-//     copyCart.total -= (Math.round(copyCart.movies[id].data.price * 100) / 100);
-//     copyCart.movies[id].quantity > 1 ?
-//     copyCart.movies[id].quantity -- :
-//     delete copyCart.movies[id];
+//     copyCart.total -= (Math.round(copyCart.Products[id].data.price * 100) / 100);
+//     copyCart.Products[id].quantity > 1 ?
+//     copyCart.Products[id].quantity -- :
+//     delete copyCart.Products[id];
 //     setCart(copyCart);
 //    }
 //    const removeItem = id => {
 //     let copyCart = {...cart};
-//     copyCart.size -= copyCart.movies[id].quantity;
-//     copyCart.total -= copyCart.movies[id].quantity * (Math.round(copyCart.movies[id].data.price * 100) / 100);
-//     delete copyCart.movies[id];
+//     copyCart.size -= copyCart.Products[id].quantity;
+//     copyCart.total -= copyCart.Products[id].quantity * (Math.round(copyCart.Products[id].data.price * 100) / 100);
+//     delete copyCart.Products[id];
 //     setCart(copyCart);
 //    }
     return (
-        <div>
-            <div className=''>
-                <div className='indcardmain'>
-                    <Card style={{ width: '21rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>product</Card.Title>
-                        <Card.Text>
-                            <p>words desc</p>
-                            <p>words desc</p>
-                            <p>words desc</p>
-                        </Card.Text>
-                            <div className='create indbutdivq'>
-                                <span><Button className="m-3" variant="secondary" > - 1 </Button></span>
-                                <span className="quan">Quantity</span>
-                                <span><Button className="m-3" variant="info" > + 1 </Button></span>
+        <Container>
+            <Row>
+                <Col></Col>
+                <Col>
+                    <div className=''>
+                    <div className='indcardmain'>
+                        <Card style={{ width: '21rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>product</Card.Title>
+                            <Card.Text>
+                                <p>words desc</p>
+                                <p>words desc</p>
+                                <p>words desc</p>
+                            </Card.Text>
+                                <div className='create indbutdivq'>
+                                    <span><Button className="m-3" variant="secondary" > - 1 </Button></span>
+                                    <span className="quan">Quantity</span>
+                                    <span><Button className="m-3" variant="info" > + 1 </Button></span>
+                                </div>
+                            <div>
+                                <div className='create indbutdiv'>
+                                    <Button className='indbut' variant="primary">Add to Cart</Button>
+                                    <Button className='indbut' variant="danger">Delete</Button>
+                                </div>
                             </div>
-                        <div>
-                            <div className='create indbutdiv'>
-                                <Button className='indbut' variant="primary">Add to Cart</Button>
-                                <Button className='indbut' variant="danger">Delete</Button>
-                            </div>
-                        </div>
-                    </Card.Body>
-                    </Card>
+                        </Card.Body>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )}
+            </Col>
+            <Col></Col>
+        </Row>
+    </Container>
+)}
 
 export default MyCart;
---
+++ src/views/MyShop.jsx
@@ -0,0 +1,92 @@
+import { useContext, useEffect, useState } from "react"
+import axios from "axios";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import { DataContext } from "../context/DataProvider";
+
+const MyShop = () => {
+    useEffect(() => { console.log('SHOP component state has been rendered or re-rendered') });
+
+
+    /*
+    1. make api call to flask ---X
+    2. set state for our component for products ---X
+    2. get products from api call and set state ----X
+    3. render on the page!
+    */
+    // async/await
+
+    const getProductData = async () => {
+        let response = await axios.get('http://127.0.0.1:5000/api/api');
+        return response.status === 200 ? response.data : null
+    }
+    const loadProductData = async () => {
+        let data = await getProductData();
+        // console.log(data)
+        setProduct(data.Product);
+    }
+    const [Product, setProduct] = useState(() => loadProductData());
+    // console.log(Product);
+
+    const { cart, setCart } = useContext(DataContext);
+
+    const addProduct = (Product) => {
+        // make a copy
+        let copyCart = { ...cart };
+        // change the copy
+        copyCart.size++;
+        copyCart.total += (Math.round(Product.price * 100) / 100);
+        copyCart.Product[Product.id] ?
+            copyCart.Product[Product.id].quantity++
+            :
+            copyCart.Product[Product.id] = { data: Product, quantity: 1 };
+        console.log(copyCart);
+        // set state
+        setCart(copyCart);
+    }
+    return (
+        <div>
+            <h1>The best store this side of the Rockies</h1>
+            <div className="container">
+                <div className="row">
+                    {console.log(Product, typeof Product)}
+                    {Product && Product.length > 0 ? Product.map((m, index) => {
+                        return <Card key={index} id={m.id} style={{ width: '13rem' }}>
+                            <Card.Img variant="top" src={m.img_url} />
+                            <Card.Body>
+                                <Card.Title>{m.title}</Card.Title>
+                                <Card.Text>
+                                    {m.desc}
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+
+                                <ListGroup.Item>Rating: {m.img}</ListGroup.Item>
+                                <ListGroup.Item>Price: ${m.price}</ListGroup.Item>
+                            </ListGroup>
+                            <Card.Body>
+                            </Card.Body>
+                            <Card.Body>
+                                <Button variant="success" onClick={() => addProduct(m)}>Add to cart</Button>
+                            </Card.Body>
+
+                        </Card>
+                    }) :
+                        <Card>
+                            <Card.Header>Sorry!</Card.Header>
+                            <Card.Body>
+                                <blockquote className="blockquote mb-0">
+                                    <p>
+                                        Sorry malfunction.
+                                    </p>
+                                </blockquote>
+                            </Card.Body>
+                        </Card>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+export default MyShop;
